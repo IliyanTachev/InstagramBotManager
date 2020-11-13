d1e6ccc222a306be301009c1b35fdd72d111c144
IliyanTachev <iliqn.tachev@gmail.com>
2020-11-13 20:52:36 +0200

    Test setup of selenium

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
+        // System Property for Chrome Driver
+        System.setProperty("webdriver.chrome.driver", "./chromedriver");
 
+        // Instantiate a ChromeDriver class.
+        WebDriver driver=new ChromeDriver();
+
+        // Launch Website
+        driver.navigate().to("https://google.com");
     }
 }
