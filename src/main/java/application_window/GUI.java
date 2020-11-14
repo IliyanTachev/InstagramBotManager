b6b71af0e72127be5d24d89b2d6a2faed08886e4
Hristian Tachev <hristian.tachev@gmail.com>
2020-11-14 20:58:03 +0200

    created GUI

--- src/main/java/application_window/GUI.java
+++ src/main/java/application_window/GUI.java
@@ -1,5 +1,45 @@
 package application_window;
 
-public class GUI {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 
+import static application_window.AppRunner.executeFTF;
+
+public class GUI{
+
+    public static JFrame createWindow() {
+        JFrame frame = new JFrame("Instagram Manager Bot");
+        frame.setSize(500, 465);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        return frame;
+    }
+
+    public static JButton createButton(String name) {
+        JButton b = new JButton(name);
+        JFrame f = createWindow();
+        int width = f.getWidth() / 2;
+        int height = 40;
+        int x = (f.getWidth() / 2) - ((f.getWidth() / 2) / 2);
+        int y = (f.getHeight() / 2) - height;
+        b.setBounds(x, y, width, height);
+        return b;
+    }
+
+    static class Action implements ActionListener {
+
+        public Action() {
+
+        }
+
+        public void actionPerformed (ActionEvent e){
+            try {
+                executeFTF();
+            } catch (FileNotFoundException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            }
+        }
+    }
 }
