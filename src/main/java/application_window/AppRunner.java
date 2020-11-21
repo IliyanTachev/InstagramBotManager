20ec89ffad2707274ba3be39cbee5127c9da6b54
IliyanTachev <iliqn.tachev@gmail.com>
2020-11-21 23:01:40 +0200

    [FIX & ADD] login bug fixed, added JavaFX plugin and dependency

--- src/main/java/application_window/AppRunner.java
+++ src/main/java/application_window/AppRunner.java
@@ -1,21 +1,17 @@
 package application_window;
 
-import functionalities.FTF;
 import classes.DriverController;
-import functionalities.impl.DefaultStrategy;
-import org.openqa.selenium.WebDriver;
+import classes.Functions;
 
 import java.io.FileNotFoundException;
-import java.sql.Driver;
-import java.util.Map;
-import java.util.Scanner;
 
 public class AppRunner {
     public static void main(String[] args) throws FileNotFoundException {
         DriverController driverController = new DriverController();
         driverController.loginToIG("phoneprincipal");
+        Functions.executeFTF(driverController);
 
-        GUI app = new GUI(driverController);
-        app.constructApplication();
+//        GUI app = new GUI(driverController);
+//        app.constructApplication();
     }
 }
